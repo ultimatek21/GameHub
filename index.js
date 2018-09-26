be97bfbfef9fcf8ab3162a59a4a8077c4efa52f1
ultimatek21 <43613163+ultimatek21@users.noreply.github.com>
2018-09-26 15:59:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const Commando = require("discord.js-commando");
+const Discord = require("discord.js");
+const bot = new Commando.Client()
+
+const TOKEN = "NDkzNDA1MDU1OTAzMzM0NDEw.DoqNiw.JRcvoGucIkEpGZ9hgKnbajaEkrk"
+console.log(bot.registry.registerGroup)
+
+
+bot.registry.registerGroup("simple", "Simple");
+bot.registry.registerGroup("admin", "Admin");
+bot.registry.registerGroup("music", "Music");
+bot.registry.registerDefaults()
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+bot.on("message", function(message){
+    if(message.content == "hello")
+    {
+        message.reply("hello, how are you?")
+    }
+    
+});
+bot.on("message", function(message){
+    if(message.content == "Hello")
+    {
+        message.reply("hello, how are you?")
+    }
+    bot.on('ready',function(){
+        console.log("Ready");      
+      });   
+});
+bot.on("guildMemberAdd", function (member)
+{
+    member.send("welcome to the server!")
+    let memberRole = member.guild.roles.find("name", "newbie");
+    member.addRole(memberRole);
+});
+
+
+
+bot.login(TOKEN)
